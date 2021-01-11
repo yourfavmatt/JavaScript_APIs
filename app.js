c40b276d26ef4af9e26f00f2f346ec6cc31a2415
Ben <bbryant@truecoders.io>
2021-01-11 16:18:03 -0600

    Added solution to Answer branch

--- app.js
+++ app.js
@@ -2,3 +2,19 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+document.getElementById("submitSearch").addEventListener("click", () => {
+  const img = document.querySelector("img");
+  const search = document.getElementById("searchWord").value;
+
+  fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=54f0MhTKuI2nxwD9nhEvN4ifNwephcpz&s=" +
+      search,
+    { mode: "cors" }
+  )
+    .then((response) => response.json())
+    .then((res) => {
+      img.src = res.data.images.original.url;
+    })
+    .catch((response) => console.log(response));
+});
